0a3d99022f4ec49fd0ca7d5c20b4ed440c065a5a
lairdp <lairdp@users.noreply.github.com>
2022-11-16 17:27:40 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,11 +6,5 @@ app = Flask(__name__)
 def hello():
     return "Hello again from Dockerised Flask"
 
-@app.route("/route")
-def hello():
-    return "This is not the 32A"
-
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='8080')
